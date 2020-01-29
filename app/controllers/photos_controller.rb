f88954437e0e372eddb9cdcd4d60905c4f716742
Mathieu Florentin <mathieu.florentin27@gmail.com>
2020-01-29 16:25:24 +0100

    Show and index page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,10 @@
 class PhotosController < ApplicationController
+  before_setup :find_photo, only [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -8,11 +13,22 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo, notice "Votre photo a bien été téléchargée !"
+    else
+      render 'new'
+    end
   end
 
   private
+
   def photo_params
     params.require(:photo).permit(:title, :description)
+  end
+
+  def find_photo
+    @photo = Photo.find(params[:id])
   end
 
 end
